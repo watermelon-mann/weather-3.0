1d15f9a8311499ab66f99efaa267fee3db5cfa93
watermelon_mann <khachaturian.vahe@gmail.com>
2023-08-09 15:30:59 +0400

    feat: add window insets

--- app/src/main/java/com/watermelonmann/weather/MainActivity.kt
+++ app/src/main/java/com/watermelonmann/weather/MainActivity.kt
@@ -3,6 +3,7 @@ package com.watermelonmann.weather
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.watermelonmann.ui.theme.WeatherTheme
 import com.watermelonmann.weather.ui.WeatherApp
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             WeatherTheme {
                 WeatherApp()
--- app/src/main/java/com/watermelonmann/weather/ui/WeatherApp.kt
+++ app/src/main/java/com/watermelonmann/weather/ui/WeatherApp.kt
@@ -1,6 +1,9 @@
 package com.watermelonmann.weather.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.watermelonmann.cities.CitiesRoute
@@ -9,12 +12,14 @@ import com.watermelonmann.ui.component.WeatherBackground
 @Composable
 fun WeatherApp() {
     WeatherBackground {
-        val navController = rememberNavController()
-        NavHost(
-            navController = navController,
-            startDestination = CitiesRoute.route
-        ) {
-            CitiesRoute.composable(this, navController)
+        Box(modifier = Modifier.safeDrawingPadding()) {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = CitiesRoute.route
+            ) {
+                CitiesRoute.composable(this, navController)
+            }
         }
     }
 }
